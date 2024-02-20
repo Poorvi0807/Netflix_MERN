a06cd3deb99062e3b208b2855b053d0550aeafc3
Poorvi Singh Thakur <poorvisinghthakur27@gmail.com>
2024-02-20 16:32:36 +0530

    Netflix Page CSS

--- netflix-mern/src/pages/Netflix.jsx
+++ netflix-mern/src/pages/Netflix.jsx
@@ -8,7 +8,7 @@ import { AiOutlineInfoCircle } from "react-icons/ai";
 
 // import { onAuthStateChanged } from "firebase/auth";
 // import { firebaseAuth } from "../utils/firebase-config";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 // import { useSelector, useDispatch } from "react-redux";
 // import { fetchMovies, getGenres } from "../store";
 
@@ -17,14 +17,14 @@ import Slider from "../components/Slider";
 export default function Netflix() {
   
   const [isScrolled, setIsScrolled] = useState(false);
-
+  const navigate = useNavigate();
   window.onscroll=()=>{
       setIsScrolled(window.pageYOffset === 0 ? false: true);
       return () => (window.onscroll = null );
 
   };
   return (
-    <div>
+    <Container>
       <Navbar isScrolled={isScrolled} />
       <div className='hero'>
       <img
@@ -51,7 +51,55 @@ export default function Netflix() {
           </div>
         </div>
       </div>
-    </div>
+      </Container>
   )
 }
-
+const Container = styled.div`
+  background-color: black;
+  .hero {
+    position: relative;
+    .background-image {
+      filter: brightness(60%);
+    }
+    img {
+      height: 100vh;
+      width: 100vw;
+    }
+    .container {
+      position: absolute;
+      bottom: 5rem;
+      .logo {
+        img {
+          width: 100%;
+          height: 100%;
+          margin-left: 5rem;
+        }
+      }
+      .buttons {
+        margin: 5rem;
+        gap: 2rem;
+        button {
+          font-size: 1.4rem;
+          gap: 1rem;
+          border-radius: 0.2rem;
+          padding: 0.5rem;
+          padding-left: 2rem;
+          padding-right: 2.4rem;
+          border: none;
+          cursor: pointer;
+          transition: 0.2s ease-in-out;
+          &:hover {
+            opacity: 0.8;
+          }
+          &:nth-of-type(2) {
+            background-color: rgba(109, 109, 110, 0.7);
+            color: white;
+            svg {
+              font-size: 1.8rem;
+            }
+          }
+        }
+      }
+    }
+  }
+`;
