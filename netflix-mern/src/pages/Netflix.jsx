1fa0d7b32a25c2a5bdd256bb5a4c80359d25b03c
Poorvi Singh Thakur <poorvisinghthakur27@gmail.com>
2024-02-20 16:23:20 +0530

    Netflix Page Started

--- netflix-mern/src/pages/Netflix.jsx
+++ netflix-mern/src/pages/Netflix.jsx
@@ -1,6 +1,18 @@
 import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
+import backgroundImage from "../assets/home.jpg";
+import styled from "styled-components";
+import MovieLogo from "../assets/homeTitle.webp";
+import { FaPlay } from "react-icons/fa";
+import { AiOutlineInfoCircle } from "react-icons/ai";
 
+// import { onAuthStateChanged } from "firebase/auth";
+// import { firebaseAuth } from "../utils/firebase-config";
+// import { useNavigate } from "react-router-dom";
+// import { useSelector, useDispatch } from "react-redux";
+// import { fetchMovies, getGenres } from "../store";
+
+import Slider from "../components/Slider";
 
 export default function Netflix() {
   
@@ -14,6 +26,9 @@ export default function Netflix() {
   return (
     <div>
       <Navbar isScrolled={isScrolled} />
+      <div className='hero'>
+        <img />
+      </div>
     </div>
   )
 }
