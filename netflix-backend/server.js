433172e1be39f6cbc1a4b821066a027a9b99a5cc
Poorvi Singh Thakur <poorvisinghthakur27@gmail.com>
2024-02-20 17:33:00 +0530

    BackendStarted

---
+++ netflix-backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connection Successful");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
+});
