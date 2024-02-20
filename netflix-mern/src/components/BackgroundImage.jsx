100d846755aa9522669d5865a722f3d152c7596c
Poorvi Singh Thakur <poorvisinghthakur27@gmail.com>
2024-02-20 11:53:46 +0530

    FrontPage

--- netflix-mern/src/components/BackgroundImage.jsx
+++ netflix-mern/src/components/BackgroundImage.jsx
@@ -10,3 +10,12 @@ export default function BackgroundImage () {
 }
 
 
+const Container = styled.div`
+height: 100vh;
+width: 100vw;
+img {
+  height: 100vh;
+  width: 100vw;
+  
+}
+`;
--- netflix-mern/src/pages/Signup.jsx
+++ netflix-mern/src/pages/Signup.jsx
@@ -6,6 +6,7 @@ import Header from '../components/Header'
 export default function Signup(){
   return <Container>
    <BackgroundImage />
+   <div className='content'>
    <Header />
    <div className='body flex column a-center j-center'>
    <div className='text flex column'>
@@ -20,9 +21,23 @@ export default function Signup(){
    </div>
    <button>Log In</button>
    </div>
+   </div>
+   
   </Container>
   
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+position: relative;
+  .content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0,0,0,0.5);
+    heightL 100vh;
+    width:100vw;
+    display:grid;
+    grid-template-rows:15vh 85vh;
+  }
+`;
 
