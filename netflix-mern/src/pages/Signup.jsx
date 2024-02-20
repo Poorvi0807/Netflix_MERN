8a13804b94bd8e7128c394cc28bc575a58021043
Poorvi Singh Thakur <poorvisinghthakur27@gmail.com>
2024-02-20 11:16:35 +0530

    InputFields

--- netflix-mern/src/pages/Signup.jsx
+++ netflix-mern/src/pages/Signup.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import styled from "styled-components"
+import BackgroundImage from '../components/BackgroundImage'
+import Header from '../components/Header'
 
 const Signup = () => {
-  return (
-    <div>
-      Signup
-    </div>
-  )
+  return<container>
+   <BackgroundImage />
+   <Header />
+   <div className='body flex column a-center j-center'>
+   <div className='text flex column'>
+    <h1>Unlimited movies, TV shows and more</h1>
+    <h4>Watch anywhere, Cancel anytime</h4>
+    <h6>Ready to watch? Enter your email to create or restart membership</h6>
+   </div>
+   <div className='form'>
+    <input type='email' placeholder='Email Address' name='email'/>
+    <input type='password' placeholder='Email Password' name='password'/>
+   </div>
+   </div>
+  </container>
+  
 }
 
 export default Signup
