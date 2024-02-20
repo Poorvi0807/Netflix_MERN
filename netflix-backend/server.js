46e29950b2f9261d85559491f278e26a520c35e0
Poorvi Singh Thakur <poorvisinghthakur27@gmail.com>
2024-02-20 18:00:53 +0530

    Server started

--- netflix-backend/server.js
+++ netflix-backend/server.js
@@ -1,24 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
+const dotenv= require("dotenv");
+dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("DB Connection Successful");
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+const connection =async()=>{
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log("DB Connetion Successfull");
+    } catch (error) {
+        console.log(error);
+    }
+   
+}
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+app.listen(8080, async() => {
+    try {
+        await connection();
+        console.log("Server started on port 8080");
+    } catch (error) {
+        console.log(error);
+    }
+  
 });
